1cac959edec8286cdc229e0d9870dfa85d26b5b8
Frosty Senpai <FrostySenpai@users.noreply.github.com>
2023-10-15 17:07:37 +0200

    added deezer support and youtube parsing alongs side a README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to use
+
+Provide your own Youtube v3 API key in `example.env` and rename the file to `.env`
+Then you can either build or just `cargo run` and provide either a `https://open.spotify.com` or a `https://deezer.page.link` as an argument and it should fetch the given song as a youtube link.
+
+This was just another programming test for myself so I can'T say that the good is good or working 100% of the time.
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,64 @@
+#![allow(non_snake_case)]
+
 use reqwest::{self, Url, header::HeaderMap};
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use wildmatch::{self, WildMatch};
+use regex::Regex;
+use dotenv::dotenv;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Response {
+#[derive(Deserialize)]
+struct YouTubeSearchResponse {
+    items: Vec<YouTubeVideo>,
+}
 
+#[derive(Deserialize)]
+struct YouTubeVideo {
+    id: YouTubeVideoId,
+}
+
+#[derive(Deserialize)]
+struct YouTubeVideoId {
+    videoId: String,
 }
 
 fn main() {
     let url: Vec<_> = std::env::args().collect();
     if url.len() > 1 {
-        println!("First argument is  {}", url[1]);
         if WildMatch::new("https://open.spotify.com/track/*").matches(&url[1]) {
             let response = request(url[1].parse::<Url>().unwrap());
-            //println!("{:?}", response);
-            let slice = &response.unwrap()[102..160];
-            println!("{slice}");
+            let slice = &response.unwrap()[102..180];
+            let re = Regex::new(r"^>|- song and lyrics by |( \| Spot.*)").unwrap();
+            let stripped = re.replace_all(slice, "");
+            println!("{stripped}");
+            match yt_search(&stripped) {
+                Ok(Some(video_url)) => {
+                    println!("Here is your result on Youtube: {}", video_url);
+                }
+                Ok(None) => {
+                    println!("No videos found for the search term.");
+                }
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                }
+            }
 
         }
         if WildMatch::new("https://deezer.page.link/*").matches(&url[1]) {
             let response = request(url[1].parse::<Url>().unwrap());
-            println!("{:?}", response);
-            //let slice = &response.unwrap()[106..150];
-            //println!("{slice}");
+            let slice = &response.unwrap()[133..200];
+            let re = Regex::new(r"^>| -|(:.*)").unwrap();
+            let stripped = re.replace_all(slice, "");
+            match yt_search(&stripped) {
+                Ok(Some(video_url)) => {
+                    println!("Here is your result on Youtube: {}", video_url);
+                }
+                Ok(None) => {
+                    println!("No videos found for the search term.");
+                }
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                }
+            }
         }
     }
 }
@@ -33,7 +69,7 @@ fn request(url_parse: Url) -> Result<String, Box<dyn std::error::Error>> {
     let client = reqwest::blocking::Client::builder()
     .default_headers(headers)
     .user_agent("curl/8.4.0")
-    .redirect(reqwest::redirect::Policy::none())
+    .redirect(reqwest::redirect::Policy::limited(4))
     .build()
     .unwrap();
     let res = client.get(url_parse)
@@ -41,3 +77,28 @@ fn request(url_parse: Url) -> Result<String, Box<dyn std::error::Error>> {
         .text()?;
     return Ok(res);
 }
+
+
+fn yt_search(search_term: &str) -> Result<Option<String>, reqwest::Error> {
+
+    dotenv().ok();
+    let api_key = std::env::var("API_KEY").expect("API_KEY must be set");
+
+    let base_url = "https://www.googleapis.com/youtube/v3/search";
+    let url = format!(
+        "{}?q={}&key={}&maxResults=1&part=snippet",
+        base_url, search_term, api_key
+    );
+
+    let response = reqwest::blocking::get(&url)?;
+    let search_results: YouTubeSearchResponse = response.json()?;
+
+    if let Some(video) = search_results.items.first() {
+        let result_id = &video.id.videoId;
+        let video_url = format!("https://www.youtube.com/watch?v={}", result_id);
+        Ok(Some(video_url))
+    } else {
+        Ok(None)
+    }
+}
+
